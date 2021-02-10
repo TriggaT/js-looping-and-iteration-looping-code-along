da33bc8339e172cede03d0d357fea431dd37cf1f
Trayshawn <traytwebb14@yahoo.com>
2021-02-10 17:44:56 -0600

    commit tree 4f91dc7902f764891b109dc6414cbce9283871eb

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let thankYous = []
+    for (let i = 0; i < name.length; i++){
+        thankYous.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    }
+    
+    return thankYous
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);}
+}
